08d716bd7cea8e3cecdb6aa33388b637278f79a1
yang.peng <Ikexunce2021>
2021-12-17 10:33:58 +0800

    新增：新版本初始化

---
+++ 2021-api/src/main/java/com/xunce/api/TestController.java
@@ -0,0 +1,11 @@
+package com.xunce.api;
+
+import com.xunce.common.utils.TestUtils;
+
+public class TestController {
+
+
+    public static void main(String[] args) {
+        System.out.println(TestUtils.main());
+    }
+}
---
+++ 2021-common/src/main/java/com/xunce/common/utils/TestUtils.java
@@ -0,0 +1,8 @@
+package com.xunce.common.utils;
+
+public class TestUtils {
+
+    public static String main() {
+        return "这是一个测试的static方法";
+    }
+}
