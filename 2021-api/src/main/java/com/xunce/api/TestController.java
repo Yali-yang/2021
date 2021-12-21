013b76471ac56274409a77fd536e0e95941fb0fa
yang.peng <Ikexunce2021>
2021-12-21 15:53:42 +0800

    新增：线程池工具类、读取配置文件工具类、打包方式

--- 2021-api/src/main/java/com/xunce/api/TestController.java
+++ 2021-api/src/main/java/com/xunce/api/TestController.java
@@ -1,7 +1,6 @@
 package com.xunce.api;
 
 import com.alibaba.fastjson.JSON;
-//import com.xunce.common.utils.TestUtils;
 
 import java.util.Arrays;
 
@@ -10,7 +9,6 @@ public class TestController {
 
     public static void main(String[] args) {
 
-//        System.out.println(TestUtils.main());
         System.out.println(JSON.toJSONString(Arrays.asList("Hello", "golang")));
 
     }
