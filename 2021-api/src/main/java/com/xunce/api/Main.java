63a72c7b00b21b27227d786ed6188055809cba23
yang.peng <Ikexunce2021>
2022-11-24 10:25:11 +0800

    新增：新增guava布隆过滤器的使用、retry:的使用

--- 2021-api/src/main/java/com/xunce/api/Main.java
+++ 2021-api/src/main/java/com/xunce/api/Main.java
@@ -3,10 +3,14 @@ package com.xunce.api;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.*;
+import com.google.common.base.Optional;
 import com.google.common.collect.*;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
 import com.google.common.io.Files;
 import com.google.common.primitives.Ints;
 import com.jayway.jsonpath.JsonPath;
+import com.xunce.common.utils.ThreadPoolUtils;
 import net.minidev.json.JSONArray;
 import com.mysql.cj.xdevapi.JsonValue;
 import com.xunce.common.utils.GuavaCacheUtils;
@@ -15,21 +19,76 @@ import net.minidev.json.JSONArray;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
+
+    /**
+     * 在内层循环里面调用continue（或者break）后接着retry标识符，程序直接转到最外层for循环去处理了
+     * url：https://www.cnblogs.com/captainad/p/10931314.html#:~:text=%E7%BD%91%E4%B8%8A%E6%BA%9C%E8%BE%BE%E4%B8%80%E7%95%AA%E5%8F%91%E7%8E%B0,%E7%9C%8B%E7%9C%8B%E4%B8%8B%E9%9D%A2%E7%9A%84%E4%BE%8B%E5%AD%90%EF%BC%9A
+     */
     @Test
-    public void testRe() {
-        String str = "/gateone/fink/";
-        String substring = str.substring(0, str.length() - 1);
-        System.out.println();
+    public void testRetry() {
+        retry:
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(j + ", ");
+                if (j == 3) {
+                    break retry;
+                }
+            }
+        }
+        System.out.println(" >>> OK");
+    }
+
+    /**
+     * url ： https://blog.csdn.net/qq_33204709/article/details/127790693
+     * <p>
+     * 用途： 用于检索一个元素是否在一个集合中。
+     * <p>
+     * 优点：
+     * <p>
+     * 时间复杂度低，增加及查询元素的时间复杂度都是O(k)，k为Hash函数的个数；
+     * 占用存储空间小，布隆过滤器相对于其他数据结构（如Set、Map）非常节省空间。
+     * 缺点：
+     * <p>
+     * 存在误判，只能证明一个元素一定不存在或者可能存在，返回结果是概率性的，但是可以通过调整参数来降低误判比例；
+     * 删除困难，一个元素映射到bit数组上的k个位置为1，删除的时候不能简单的直接置为0，可能会影响到其他元素的判断。
+     */
+    @Test
+    public void testBloomFilter() {
+        // 初始化布隆过滤器，设计预计元素数量为1000000L，误差率为1%
+        BloomFilter<CharSequence> bloomFilter = BloomFilter.create(Funnels.stringFunnel(StandardCharsets.UTF_8), 1000000, 0.01);
+        String str = "abc";
+        boolean put = bloomFilter.put(str);
+        boolean b = bloomFilter.mightContain(str);
+        boolean g = bloomFilter.mightContain("fasdfas");
+
+//        int n = 1000000;
+//        for (int i = 0; i < n; i++) {
+//            bloomFilter.put(String.valueOf(i));
+//        }
+//        int count = 0;
+//        for (int i = 0; i < (n * 2); i++) {
+//            if (bloomFilter.mightContain(String.valueOf(i))) {
+//                count++;
+//            }
+//        }
+//        System.out.println("过滤器误判率：" + 1.0 * (count - n) / n);
     }
 
     @Test
